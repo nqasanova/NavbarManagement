a92b81d9008f41c7b608ec4a125ed26d857dc377
Natavan Hasanova <natavanhesenova4@gmail.com>
2022-12-05 11:50:12 +0400

    Navbar ListItemViewModel updated and added to the system.

--- NavbarManagement/NavbarManagement/ViewModels/Admin/Navbar/ListItemViewModel.cs
+++ NavbarManagement/NavbarManagement/ViewModels/Admin/Navbar/ListItemViewModel.cs
@@ -1,19 +1,22 @@
 ﻿using System;
+
 namespace NavbarManagement.ViewModels.Admin.Navbar
 {
-    public class ListItemViewModel
+    public class ListItemViewModel 
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string URL { get; set; }
         public int Order { get; set; }
         public bool IsBold { get; set; }
         public bool IsHeader { get; set; }
         public bool IsFooter { get; set; }
 
-        public ListItemViewModel(int id, string name, int order, bool isBold, bool isHeader, bool isFooter)
+        public ListItemViewModel(int id, string name, string url, int order, bool isBold, bool isHeader, bool isFooter)
         {
             Id = id;
             Name = name;
+            URL = url;
             Order = order;
             IsBold = isBold;
             IsHeader = isHeader;
